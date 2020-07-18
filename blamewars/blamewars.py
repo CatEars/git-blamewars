2825660c994784ab79e35db284d7b81877e567ff
Henke Adolfsson <catears13@gmail.com>
2020-07-18 20:19:11 +0200

    Add testcode that runs the whole pipeline

--- blamewars/blamewars.py
+++ blamewars/blamewars.py
@@ -6,3 +6,13 @@ def open_in_browser(html_file: str):
         f.write(html_file)
         url = 'file://{}'.format(f.name)
     webbrowser.open(url)
+
+
+if __name__ == '__main__':
+    import os
+    import gitstats
+    import template
+    current_dir = os.getcwd()
+    blame_stats = gitstats.blame_stats_from_repo(current_dir)
+    html = template.generate_template(blame_stats)
+    open_in_browser(html)
