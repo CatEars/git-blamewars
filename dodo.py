f2b6247f50c3ca73152201c28db17db06a6d3429
Henke Adolfsson <catears13@gmail.com>
2020-07-18 11:25:09 +0200

    Remove coverage task

--- dodo.py
+++ dodo.py
@@ -6,10 +6,3 @@ def task_test():
         'actions': ['python -m pytest'],
         'verbosity': STDOUT
     }
-
-
-def task_coverage():
-    return {
-        'actions': ['python -m pytest --cov=git-blamewars --cov-report=html --cov-branch'],
-        'verbosity': STDOUT
-    }
