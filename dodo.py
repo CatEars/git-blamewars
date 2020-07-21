c6504d469b3d052e9c7bd1073f9d09a586175e8d
Henke Adolfsson <catears13@gmail.com>
2020-07-21 21:52:56 +0200

    Add coverage task

--- dodo.py
+++ dodo.py
@@ -6,3 +6,10 @@ def task_test():
         'actions': ['python -m pytest'],
         'verbosity': STDOUT
     }
+
+
+def task_coverage():
+    return {
+        'actions': ['python -m pytest --cov-report html --cov=blamewars --cov-branch'],
+        'verbosity': STDOUT
+    }
